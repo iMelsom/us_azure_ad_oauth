8718539be32c52041afc96b5a7c4057987999e3a
ian <ian@Idoru>
2026-01-30 14:59:14 +0100

    aerfds

--- hooks/loginbottom.php
+++ hooks/loginbottom.php
@@ -1,5 +1,5 @@
 <?php
-error_log("blipp from loginbottom hook");
+//error_log("blipp from loginbottom hook");
 if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted 
 require_once '../users/init.php';
 global $settings, $user, $db, $authorize_url, $us_url_root, $abs_us_root;
@@ -7,7 +7,7 @@ if($settings->aao_login==1 && !$user->isLoggedIn()){
 ?>
   <div class="col-sm-6 text-center">
   <br/>
-  <?PHP require_once $abs_us_root.$us_url_root.'usersc/plugins/azure_ad_oauth/assets/aao_oauth_login.php'; ?>
+  <?PHP require_once $abs_us_root.$us_url_root.'usersc/plugins/us_azure_ad_oauth/assets/aao_oauth_login.php'; ?>
   </div>
 <?PHP
   }
