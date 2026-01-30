e505a69ebb48c44437b8e636218e7b9c087e5299
ian <ian@Idoru>
2026-01-30 15:15:31 +0100

    aerfd

--- hooks/logout.php
+++ hooks/logout.php
@@ -4,7 +4,7 @@ require_once '../users/init.php';
 global $settings, $user, $db, $authorize_url, $us_url_root, $abs_us_root;
 
 if($user->data()->aao_active == 1 && $user->isLoggedIn()){
-    header('Location:https://' . $_SERVER['SERVER_NAME']. '/usersc/plugins/us_azure_ad_oauthazure_ad_oauth/assets/?action=logout');
+    header('Location:https://' . $_SERVER['SERVER_NAME']. '/usersc/plugins/us_azure_ad_oauth/assets/?action=logout');
     $db->query('UPDATE users SET aao_active = 0 WHERE id = ?', [$user->data()->id]);
     exit;
 }
