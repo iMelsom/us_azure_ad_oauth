b5c7160f1c9491cde67afffb8cefabcb117704ea
ian <ian@Idoru>
2026-01-30 14:52:22 +0100

    aerwdf

--- plugin_info.php
+++ plugin_info.php
@@ -1,4 +1,4 @@
 <?php
 //Use this file to define info that you want to use throughout your plugin. You must declare folder name and plugin name
-$plugin_name = "azure_ad_oauth"; //should be the same as the folder name in all lowercase.
+$plugin_name = "us_azure_ad_oauth"; //should be the same as the folder name in all lowercase.
  ?>
